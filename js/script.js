15f06ebdccedd9324df553d9068633af62188467
Andre I <aisaac2020@bklawtech.com>
2018-04-18 21:21:49 +0000

    search term 1 + search term 2

--- js/script.js
+++ js/script.js
@@ -13,24 +13,36 @@ $(document).ready(function(){
 function appendImageToBody(srcURL) {
     // write a function that will append an <img> to the body with the
     // URL provided in the parameters
-    $('body').append('<img src=' + srcURL + '>');
+    $('#giphy').append('<img src=' + srcURL + '>');
 }
 
 function callGiphyAPIWithSearchTerm(searchTerm) {
     $.ajax({
       url: "https://api.giphy.com/v1/stickers/search?q="+searchTerm+"&api_key=dc6zaTOxFJmzC",
       method: "GET",
-      success: function(response) {
+      success: function(response) { 
+          if(response.data.length > 0){
            var url = response.data[0].images.original.url;
            appendImageToBody(url);
+              
+          }else{
+              
+              $('#giphy').append('<h1>' + 'Can not find your search term(s).' + '</h1>');
+              
+          }
+           
       },
     }); 
 }
 
 //Add a click handler below to call the function when the button is clicked
-$("button").click(function() {
-    var searchTerm = $("input").val();
-    callGiphyAPIWithSearchTerm(searchTerm);
+$("#button1").click(function() {
+    
+    var searchTerm1 = $("#input1").val();
+    var searchTerm2 = $("#input2").val();
+    callGiphyAPIWithSearchTerm(searchTerm1+searchTerm2);
+
+    
 });
   
   
